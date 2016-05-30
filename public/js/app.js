b7aad8820784c9efb8e01fe38f39ced9beff6582
greeneat <greeneat2@hotmail.com>
2016-05-30 20:44:04 +0700

    Costume Message Event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect',function(){
   console.log('Connected socket.io on server');  
 });
+
+socket.on('message',function(message){
+  console.log("new message");
+  console.log(message.text);
+});
